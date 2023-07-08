4b271d924bc1bb5a8aa1373305a5c3e148dddd5f
Yuqiang Wang <2053731441@qq.com>
2023-07-09 07:26:18 +0800

    [submit] 2023.7.9 (#39)
    1.LM386逻辑代码
    2.GP2Y逻辑代码
    3.学习笔记

--- RTT-Frame/5.Sensor code/GP2Y-PM2.5/air_quality_mtor.h
+++ RTT-Frame/5.Sensor code/GP2Y-PM2.5/air_quality_mtor.h
@@ -5,20 +5,18 @@
  *
  * Change Logs:
  * Date           Author       Notes
- * 2023-04-25     Yifang       the first version
+ * 2023-07-08     Yifang       the first version
  */
-#ifndef APPLICATIONS_GP2Y_H_
-#define APPLICATIONS_GP2Y_H_
+#ifndef APPLICATIONS_AIR_QUALITY_MTOR_H_
+#define APPLICATIONS_AIR_QUALITY_MTOR_H_
 
 #include <rtthread.h>
 #include <rtdevice.h>
 #include <board.h>
 
-#define GP2Y_PIN    GET_PIN(A,12)
+#define GP2Y_PIN    GET_PIN(B,13)
 
 #define GP2Y_High rt_pin_write(GP2Y_PIN,PIN_HIGH)
 #define GP2Y_Low  rt_pin_write(GP2Y_PIN,PIN_LOW)
 
-void GP2Y_Value_read(void);
-
-#endif /* APPLICATIONS_GP2Y_H_ */
+#endif /* APPLICATIONS_AIR_QUALITY_MTOR_H_ */
